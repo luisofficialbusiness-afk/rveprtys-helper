75411a83564f03f88d0c514f0dd01051cf4a5489
RVEPRTY <luisofficialbusiness@gmail.com>
2026-05-30 12:56:26 -0500

    Add lastDaily and dailyStreak fields to User model

--- models/User.js
+++ models/User.js
@@ -6,7 +6,9 @@ const userSchema = new mongoose.Schema({
     balance: { type: Number, default: 0 },
     bank: { type: Number, default: 0 },
     lastWork: { type: Number, default: 0 },
-    lastRob: { type: Number, default: 0 }
+    lastRob: { type: Number, default: 0 },
+    lastDaily: { type: Number, default: 0 },
+    dailyStreak: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('User', userSchema);
