c815f717a01b108aeb65c039f112322e854ccd3e
RVEPRTY <luisofficialbusiness@gmail.com>
2026-06-02 14:16:51 -0500

    Add prestige and prestigeMultiplier fields to user model

--- src/models/user.js
+++ src/models/user.js
@@ -10,6 +10,8 @@ const userSchema = new mongoose.Schema({
     lastDaily: { type: Number, default: 0 },
     dailyStreak: { type: Number, default: 0 },
     dmOptOut: { type: Boolean, default: false },
+    prestige: { type: Number, default: 0 },
+    prestigeMultiplier: { type: Number, default: 1 }
 });
 
 module.exports = mongoose.model('User', userSchema);
