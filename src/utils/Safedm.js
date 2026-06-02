9f68122c21959367b0cf50412e9ee2a8442b6318
RVEPRTY <luisofficialbusiness@gmail.com>
2026-06-01 21:54:56 -0500

    Add safeDM function for sending direct messages

---
+++ src/utils/Safedm.js
@@ -0,0 +1,15 @@
+const User = require('../models/user');
+
+async function safeDM(client, userId, payload) {
+    try {
+        const user = await User.findOne({ userId });
+        if (user?.dmOptOut) return false;
+        const discordUser = await client.users.fetch(userId);
+        await discordUser.send(payload);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+module.exports = { safeDM };
